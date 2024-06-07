ef1148f85f491fad19d01d131b4a20a77bb188bc
AbdulAhad7553 <25100016@lums.edu.pk>
2024-06-07 17:29:53 +0500

    config update API_BASE_URL

--- frontend/src/config/config.tsx
+++ frontend/src/config/config.tsx
@@ -1,3 +1,3 @@
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://mine-court-track-6sgd.vercel.app/';
 const CLOUDINARY_BASE_URL = 'https://res.cloudinary.com/dm56xy1oj/image/upload/v1717585302/';
 export { API_BASE_URL, CLOUDINARY_BASE_URL };
