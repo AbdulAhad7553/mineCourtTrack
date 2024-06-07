a990b2826bc9038236c24a6abe3b2ceb82ddd1b8
AbdulAhad7553 <25100016@lums.edu.pk>
2024-06-07 17:36:44 +0500

    backend server.js and app.js updated

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,52 @@
 
 
 
+// import { Server } from "socket.io";
+// import http from "http";
+// import { app } from "./app.js";
+// import { config } from "dotenv";
+
+// config({
+//   path: "./config.env",
+// });
+
+// // Create HTTP server using the Express app
+// const server = http.createServer(app);
+
+// // Setup Socket.IO server
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:5173",
+//     methods: ["GET", "POST"],
+//   },
+// });
+
+// io.on("connection", (socket) => {
+//   console.log("USER CONNECTED:", socket.id);
+// });
+
+// // Start the HTTP server and the Socket.IO server on the same port
+// const PORT = process.env.PORT || 8000;
+// server.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import { Server } from "socket.io";
 import http from "http";
 import { app } from "./app.js";
@@ -40,7 +86,7 @@ const server = http.createServer(app);
 // Setup Socket.IO server
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://mine-court-track.vercel.app", // Your frontend URL
     methods: ["GET", "POST"],
   },
 });
