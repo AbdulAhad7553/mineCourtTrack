29df761f49e40e887659561e0271f33a6887a405
AbdulAhad7553 <25100016@lums.edu.pk>
2024-06-14 11:20:35 +0500

    Update RemoveTeams.tsx

--- frontend/src/pages/RemoveTeams.tsx
+++ frontend/src/pages/RemoveTeams.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { API_BASE_URL } from "../config/config";
 import Navbar from "../components/navBar_componets/Navbar";
@@ -30,8 +29,7 @@ interface Player {
 const RemoveTeams = () => {
   const [teams, setTeams] = useState<Team[]>([]);
   const [selectedTeam, setSelectedTeam] = useState<Team | null>(null);
-  const navigate = useNavigate();
-
+  
   useEffect(() => {
     const fetchTeams = async () => {
       try {
